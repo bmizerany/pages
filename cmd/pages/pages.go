73ad40521502ce126e69bf892821cc1882364fde
Blake Mizerany <blake.mizerany@gmail.com>
2021-12-25 03:09:51 -0800

    take in data via plugin

--- cmd/pages/pages.go
+++ cmd/pages/pages.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"plugin"
 
 	"blake.io/pages"
 )
@@ -11,7 +12,10 @@ import (
 var (
 	flagVerbose      = flag.Bool("v", false, "enable verbose logging")
 	flagRemovePublic = flag.Bool("rm", false, "forcefully remove ./public")
+	flagPlugin       = flag.String("p", "", "load funcs and data from Go plugin")
 )
+
+// TODO(bmizerany): load JSON data from pages.json if -p not set
 
 func main() {
 	log.SetFlags(0)
@@ -22,6 +26,23 @@ func main() {
 	cfg := &pages.Config{}
 	if *flagVerbose {
 		cfg.Logf = log.Printf
+	}
+
+	if *flagPlugin != "" {
+		p, err := plugin.Open(*flagPlugin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		funcs, err := p.Lookup("Funcs")
+		if err != nil {
+			log.Fatal(err)
+		}
+		data, err := p.Lookup("Data")
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfg.Funcs = *funcs.(*map[string]interface{})
+		cfg.Data = data
 	}
 
 	if *flagRemovePublic {
